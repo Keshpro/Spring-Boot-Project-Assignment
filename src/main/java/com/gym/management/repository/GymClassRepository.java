4680566fbf9f62ee6b2a28bc128685b5281df3df
Keshan Panditharathna <keshanebusiness@gmail.com>
2026-01-15 23:34:15 +0530

    error files

--- src/main/java/com/gym/management/repository/GymClassRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gym.management.repository;
-
-import com.gym.management.entity.GymClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GymClassRepository extends JpaRepository<GymClass, Long> {
-    // දැනට විශේෂ මෙතඩ් ඕන නැහැ, CRUD ඔක්කොම JpaRepository එකෙන් ලැබෙනවා.
-}
